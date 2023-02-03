c87f80ab2c5393b05eb14cb804daa7d1932be439
Keerthimalini6 <keerthimalini6699@gmail.com>
2023-02-03 10:59:38 +0530

    Pre loader added to Home page

--- scripts/home-page.js
+++ scripts/home-page.js
@@ -1,3 +1,25 @@
+var loader;
+
+function loadNow(opacity) {
+    if (opacity <= 0) {
+        displayContent();
+    } else {
+        loader.style.opacity = opacity;
+        window.setTimeout(function() {
+            loadNow(opacity - 0.05);
+        }, 100);
+    }
+}
+
+function displayContent() {
+    loader.style.display = 'none';
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    loader = document.getElementById('loader');
+    loadNow(1);
+});
+
 //navbar and footer import
 
 import {navbar} from "../components/navbar/navbar.js";
