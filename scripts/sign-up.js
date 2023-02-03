8e753ea79ab1b3e579c66e71d6ed9bd110722176
Anand R P <anand.rp2030@gmail.com>
2023-02-03 19:46:18 +0530

    sign up progress

--- scripts/sign-up.js
+++ scripts/sign-up.js
@@ -1,7 +1,19 @@
 
-var formData = JSON.parse(localStorage.getItem("userData")) || [];
-
+var userDataArr = JSON.parse(localStorage.getItem("userData")) || [];
 let form = document.querySelector("#form-id");
+let passwordMatch = false;
+
+class UserData{
+    constructor(userName, place, email, pass, confirmPass){
+        this.userName = userName;
+        this.place = place;
+        this.email = email;
+        this.pass = pass;
+        this.confirmPass = confirmPass;
+    }
+}
+
+
 
 form.addEventListener("submit", function (e){
     e.preventDefault()
@@ -12,8 +24,9 @@ form.addEventListener("submit", function (e){
     let pass = document.querySelector("#signin-password").value;
     let confirmPass = document.querySelector("#confirm-password").value;
 
+  
 
-
+    
 
     let userNameWarn = document.querySelector(".username-warning");
     let placeWarn = document.querySelector(".place-warning");
@@ -49,28 +62,41 @@ form.addEventListener("submit", function (e){
         passWarn.classList.remove("display-block");
     }
 
-    
+    if (userName, place, email, pass, confirmPass, passwordMatch){
+        
+        let user = new UserData (userName, place, email, pass, confirmPass)
+        userDataArr.push(user);
+        localStorage.setItem("userData",JSON.stringify(userDataArr));
+        sendOtpToMail(user)
 
-
-    // if (!password){
-    //     passwordWarn.classList.add("display-block");
-    // }else {
-    //     passwordWarn.classList.remove("display-block");
-    // }
-
-
-    // if (userName && Email && phonenumber && password){
-    //     let userData = new userConstructor (userName, Email, phonenumber, password)
-    //     localStorage.setItem("user", JSON.stringify(userData));
-
-    //     // changeMap(cityName);
-    // }
+        window.location.href = "../pages/otp.html";
+    }
     
 })
 
-let confirmInput = document.querySelector("#confirm-password");
-confirmInput.addEventListener("input", function (){
+let pass = document.querySelector("#signin-password");
 
+pass.addEventListener("input", typingPass);
+function typingPass (){
+
+    let pass = document.querySelector("#signin-password").value;
+    let passWarn = document.querySelector(".pass-warning");
+    if (pass.length < 6){
+        passWarn.classList.add("display-block")
+    }else {
+        passwordMatch = true;
+        passWarn.classList.remove("display-block")
+    }
+
+}
+
+
+
+
+let confirmInput = document.querySelector("#confirm-password");
+confirmInput.addEventListener("input", checkPass);
+
+function checkPass (){
 
     let pass = document.querySelector("#signin-password").value;
     let confirmPassWarn = document.querySelector(".confirm-warning");
@@ -81,19 +107,54 @@ confirmInput.addEventListener("input", function (){
         if (pass != confirmPass){
             confirmPassWarn.classList.add("display-block");
         }else {
-            confirmPassWarn.classList.remove("display-block");
-            
+            // confirmPassWarn.classList.remove("display-block");
+            confirmPassWarn.innerHTML = "Password match";
+            confirmPassWarn.classList.add("password-match")
         }
     }
-}) 
+}
+
+// sending otp 
+
+let otpArr = JSON.parse(localStorage.getItem("otp")) || [];
+
+// otp feature
+function sendOtpToMail(userDataObj) {
+  let name = userDataObj.userName;
+  let mail = userDataObj.email;
+  let otp = generateOTP();
+
+  let otpObj = {
+    name: name,
+    mail: mail,
+    otp: otp,
+  };
+
+  otpArr.push(otpObj);
+  localStorage.setItem("otp", JSON.stringify(otpArr));
+
+  const serviceId = "service_f3nv3am";
+  const templateId = "template_xwrbdho";
+  const apiKey = "8VnLYUKesp8CehjBF";
 
 
-function userConstructor (userName, Email, phonenumber, password){
-    this.userName = userName;
-    this.Email = Email;
-    this.phonenumber = phonenumber;
-    this.password = password;
-  }
-  formData.push(userConstructor);
+  emailjs
+    .send(serviceId, templateId, { name, mail, otp }, apiKey)
+    .then(() => {
+        alert('otp send to mail')
+    })
+    .catch((error) => {
+      console.error(error);
+      alert("There was an error sending the email. Please try again later.");
+    });
+}
+
+//generate 4 digits otp
+function generateOTP() {
+  var otp = Math.floor(Math.random() * 9000) + 1000;
+  return otp;
+}
+
+
 
  
