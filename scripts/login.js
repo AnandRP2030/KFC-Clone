8b754e35c9185a28a95cb84922db629728203dd9
Anand R P <anand.rp2030@gmail.com>
2023-02-04 03:15:18 +0530

    changges

---
+++ scripts/login.js
@@ -0,0 +1,11 @@
+let userEmail = document.querySelector(".email-input").value;
+let userPass = document.querySelector(".signin-password").value;
+
+
+let loginBtn = document.querySelector(".sign-up-submit");
+loginBtn.addEventListener ("onclick", function (){
+alert('c')
+    console.log(userEmail, userPass)
+    
+})
+
